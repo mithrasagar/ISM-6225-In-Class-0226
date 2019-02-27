1e6a3ad7241d28e388da580dd764d9946350a17e
mithrasagar <46505671+mithrasagar@users.noreply.github.com>
2019-02-26 21:25:32 -0500

    Updated with foreign key code

--- dbo.Vehicle.sql
+++ dbo.Vehicle.sql
@@ -1,19 +1,23 @@
-USE [Insurance]
+﻿USE [Insurance]
 GO
 
-/****** Object: Table [dbo].[Vehicle] Script Date: 2/26/2019 9:14:24 PM ******/
+/****** Object: Table [dbo].[Vehicle] Script Date: 2/26/2019 9:24:30 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Vehicle] (
-    [ID]    INT          NOT NULL,
-    [Make]  VARCHAR (50) NOT NULL,
-    [Model] VARCHAR (50) NOT NULL,
-    [Year]  CHAR (10)    NOT NULL,
-    [Color] CHAR (10)    NOT NULL
-);
+CREATE TABLE [dbo].[Vehicle]
+(
+	[VehicleNo] INT NOT NULL , 
+    [Make] VARCHAR(50) NOT NULL, 
+    [Model] VARCHAR(50) NOT NULL, 
+    [Year] CHAR(10) NOT NULL, 
+    [Color] CHAR(10) NOT NULL, 
+    [OwnerNo] INT NOT NULL, 
+    CONSTRAINT [FK_Vehicle_Owner_OwnerNo] FOREIGN KEY (OwnerNo) REFERENCES [Owner]([OwnerNo]), 
+    CONSTRAINT [PK_Vehicle] PRIMARY KEY ([VehicleNo]) 
+)
 
 
